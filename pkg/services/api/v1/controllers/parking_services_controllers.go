0731bc9b107afdfec86eb627b0e59e027c942ab7
dickygeraldi <dickygeraldi@gmail.com>
2020-02-26 02:52:09 +0700

    third commit

--- pkg/services/api/v1/controllers/parking_services_controllers.go
+++ pkg/services/api/v1/controllers/parking_services_controllers.go
@@ -3,6 +3,9 @@ package controllers
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net"
 	"os"
 	v1 "proto-parking-services/pkg/api/v1"
 	"proto-parking-services/pkg/services/api/v1/models"
@@ -17,6 +20,47 @@ import (
 // UserServices implemented on version 1 proto interface
 type parkingServices struct {
 	db *sql.DB
+}
+
+// socket connection handle
+func socketHandle() {
+	l, err := net.Listen("tcp", os.Getenv("SOCKET_HOST"))
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+
+	defer l.Close()
+	for {
+		// listen all incoming message
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		// Handle connections in a new goroutine.
+		go handleRequest(conn)
+	}
+}
+
+// Socket handle request
+func handleRequest(conn net.Conn) {
+	buf := make([]byte, 1024)
+
+	_, err := conn.Read(buf)
+	data := make(map[string]interface{})
+
+	json.Unmarshal(buf, &data)
+	fmt.Println(data)
+
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+
+	// Send a response back to person contacting us.
+	conn.Write([]byte("Message received."))
+	// Close the connection when you're done with it.
+	conn.Close()
 }
 
 // New sending otp services create sending otp service
