7626d377505e09adddda0dd4b4d4108c7346216b
dickygeraldi <dickygeraldi@gmail.com>
2020-02-25 22:59:04 +0700

    first commit

--- pkg/services/api/v1/validation/validation.go
+++ pkg/services/api/v1/validation/validation.go
@@ -3,16 +3,15 @@ package validation
 import (
 	"proto-parking-services/pkg/services/api/v1/logging"
 	"regexp"
-	"time"
 )
 
 var re = regexp.MustCompile("select|insert|update|alter|delete")
 
 // Function for validation user services request
-func ParkingRegistration(api, platNo, dataIp string, dateTime time.Time) (string, bool) {
+func ParkingRegistration(api, platNo, dataIp string, dateTime string) (string, bool) {
 	if api != "" && platNo != "" {
 		if re.MatchString(api) == true || re.MatchString(platNo) == true {
-			go logging.SetLogging("Parking Request", dataIp, "SQL Injection", "Warning message", "warning", "SQL Injection in this request", dateTime.String())
+			go logging.SetLogging("Parking Request", dataIp, "SQL Injection", "Warning message", "warning", "SQL Injection in this request", dateTime)
 			return "Coba lagi nanti, transaksi di pending", false
 		} else {
 			return "", true
