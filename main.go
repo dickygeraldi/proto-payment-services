a460429b9d6f9f5123f1aacd10fee765f7325e46
dickygeraldi <dickygeraldi@gmail.com>
2020-02-26 19:54:35 +0700

    Adding Socket

--- main.go
+++ main.go
@@ -4,14 +4,9 @@ import (
 	"fmt"
 	"os"
 	cmd "proto-parking-services/pkg/cmd/server"
-	"proto-parking-services/pkg/services/api/v1/models"
 )
 
 func main() {
-
-	go func() {
-		models.HandleSocket()
-	}()
 
 	if err := cmd.RunServer(); err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
